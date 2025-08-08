8b0f1afe640a461eb67ee3c14b444f6829e94d82
PrayaaDikk <fadilprayadika@gmail.com>
2025-08-09 07:13:35 +0800

    modified: font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { DM_Sans } from "next/font/google";
+
+const dmSans = DM_Sans({
+    subsets: ["latin"],
+    weight: ["400", "500", "600", "700"],
+    display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <main className={dmSans.className}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
