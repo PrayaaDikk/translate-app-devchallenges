fce5490efc25e54c7cf43b56f471ebfbe735a6cb
PrayaaDikk <fadilprayadika@gmail.com>
2025-08-09 07:14:51 +0800

    modified: add title for page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,40 @@
 import TranslateForm from "@/components/TranslateForm";
 import Image from "next/image";
+import Head from "next/head";
 
 const Home = () => {
     return (
-        <section className="bg-blackTheme relative min-h-[100dvh] overflow-hidden">
-            <Image
-                src="/hero_img.jpg"
-                alt="hero-background"
-                className="absolute min-w-[1000px]"
-                width={1920}
-                height={1080}
-                loading="eager"
-                priority
-            />
-            <main className="relative z-10 mx-auto w-full max-w-[1080px] space-y-2 px-4 xl:mt-10">
-                {/* logo */}
-                <div className="flex items-center justify-center p-12 *:size-auto">
-                    <Image
-                        src="/logo.svg"
-                        alt="logo"
-                        width={120}
-                        height={120}
-                        loading="eager"
-                    />
-                </div>
+        <>
+            <Head>
+                <title>Translate App Devchallenges</title>
+            </Head>
+            <section className="bg-blackTheme relative min-h-[100dvh] overflow-hidden">
+                <Image
+                    src="/hero_img.jpg"
+                    alt="hero-background"
+                    className="absolute min-w-[1000px]"
+                    width={1920}
+                    height={1080}
+                    loading="eager"
+                    priority
+                />
+                <main className="relative z-10 mx-auto w-full max-w-[1080px] space-y-2 px-4 xl:mt-10">
+                    {/* logo */}
+                    <div className="flex items-center justify-center p-12 *:size-auto">
+                        <Image
+                            src="/logo.svg"
+                            alt="logo"
+                            width={120}
+                            height={120}
+                            loading="eager"
+                        />
+                    </div>
 
-                {/* form */}
-                <TranslateForm />
-            </main>
-        </section>
+                    {/* form */}
+                    <TranslateForm />
+                </main>
+            </section>
+        </>
     );
 };
 
