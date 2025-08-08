625ed44409677b946196a9a9c240b52689de3296
PrayaaDikk <fadilprayadika@gmail.com>
2025-08-09 07:13:54 +0800

    modified: metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,23 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <meta
+                    name="description"
+                    content="A fast and easy translator app with multiple languages support."
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <meta charSet="UTF-8" />
+            </Head>
+            <body className="antialiased">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
